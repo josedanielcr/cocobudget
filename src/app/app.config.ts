2fa6e266e5841f4f51db1ac19564be4ffd876771
josedanielcr <jdcanales1905@gmail.com>
2024-10-14 17:38:50 -0600

    Feat: authentication added to B2C

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -22,12 +22,31 @@ export function loggerCallback(logLevel: LogLevel, message: string) {
   console.log(message);
 }
 
+export const b2cPolicies = {
+  names: {
+    signUpSignIn: 'B2C_1_susi',
+    resetPassword: 'B2C_1_password_reset'
+  },
+  authorities: {
+    signUpSignIn: {
+      authority:
+        'https://cocobudget.b2clogin.com/cocobudget.onmicrosoft.com/B2C_1_cocobudget_signin_signup',
+    },
+    resetPassword: {
+      authority:
+        'https://cocobudget.b2clogin.com/cocobudget.onmicrosoft.com/B2C_1_password_reset_flow',
+    }
+  },
+  authorityDomain: 'cocobudget.b2clogin.com',
+};
+
 //configuration
 export function MSALInstanceFactory(): IPublicClientApplication {
   return new PublicClientApplication({
     auth: {
-      clientId: environment.msalConfig.auth.clientId,
-      authority: environment.msalConfig.auth.authority,
+      clientId: environment.adb2cConfig.clientId,
+      authority: b2cPolicies.authorities.signUpSignIn.authority, //environment.msalConfig.auth.authority,
+      knownAuthorities: [b2cPolicies.authorityDomain], // Mark your B2C tenant's domain as trusted.
       redirectUri: '/',
       postLogoutRedirectUri: '/',
     },
@@ -48,8 +67,8 @@ export function MSALInstanceFactory(): IPublicClientApplication {
 export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
   const protectedResourceMap = new Map<string, Array<string>>();
   protectedResourceMap.set(
-    environment.apiConfig.uri,
-    environment.apiConfig.scopes
+    environment.adb2cConfig.apiEndpointUrl,
+    environment.adb2cConfig.scopeUrls
   );
   return {
     interactionType: InteractionType.Redirect,
@@ -61,7 +80,7 @@ export function MSALGuardConfigFactory(): MsalGuardConfiguration {
   return {
     interactionType: InteractionType.Redirect,
     authRequest: {
-      scopes: [...environment.apiConfig.scopes],
+      scopes: [...environment.adb2cConfig.scopeUrls],
     },
     loginFailedRoute: '/login-failed',
   };
