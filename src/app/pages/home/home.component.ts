e248a4d9054351f2f7b746023b8246256708b182
josedanielcr <jdcanales1905@gmail.com>
2024-10-16 00:39:36 -0600

    Feat: nav bar structure

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {MatDrawer, MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule, MatButtonModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  @ViewChild('drawer') drawer : MatDrawer | undefined;
+
+  isExpanded : boolean = true;
+
+  toggleNav() {
+    this.drawer?.toggle();
+    this.isExpanded = !this.isExpanded;
+  }
 }
