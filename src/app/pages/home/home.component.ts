696ba462a0e1d0a1164c064f202af773e68bddd3
josedanielcr <jdcanales1905@gmail.com>
2024-10-16 22:08:28 -0600

    Feat: general styles added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,17 +3,20 @@ import {RouterOutlet} from '@angular/router';
 import {MatDrawer, MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, MatSidenavModule, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet, MatSidenavModule, MatButtonModule, MatIconModule, NgClass],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
   @ViewChild('drawer') drawer : MatDrawer | undefined;
+  isOptionHovered : boolean = false;
 
   isExpanded : boolean = true;
 
